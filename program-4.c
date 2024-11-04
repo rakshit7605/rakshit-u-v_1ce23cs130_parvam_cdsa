be55177f8ee91f231375a80096f5f15f805cec7d
rakshit7605 <rakshituv@outlook.com>
2024-11-04 12:16:39 +0530

    Add files via upload

---
+++ program-4.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a=10;
+    printf("%d\n",a);
+    float b=10.3223;
+    printf("%.2f\n",b);
+    char c='a';
+    printf("%c\n",c);
+    char s[]="hello";
+    printf("%s\n",s);
+}
